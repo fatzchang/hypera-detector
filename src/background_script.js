d7cef5adf4cd82d54a2b1903d383a041ec213817
fatzchang <fatz.zhang@gmail.com>
2022-05-04 12:42:36 +0800

    establish ws connection per tab

--- src/background_script.js
+++ src/background_script.js
@@ -5,45 +5,59 @@ const wsState = {
   CLOSED: 3
 };
 
-
-const videoList = {}
-let ws;
+wsList = {}; // group by tabid
 
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const tryConnect = async () => {
-  while (!ws || ws.readyState !== wsState.OPEN) {
-    console.log('try connecting...');
-    ws = new WebSocket('ws://localhost:49367/ws');
-    await sleep(5000);
-  }
-  
-  console.log('connected!');
+chrome.tabs.onCreated.addListener((tab) => {
+  console.log('tab is created!');
+  const videoList = {};
+  let ws = wsList[tab.id];
 
-  ws.onclose = () => {
-    console.log('disconnected!');
-    tryConnect();
-  };
-}
-
-tryConnect();
-
-// this api is asynchronous
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    if (ws && ws.readyState === wsState.OPEN) {
-      if (!videoList[details.url] && details.url.includes('m3u8')) {
-  
-        videoList[details.url] = true;
-        ws.send(JSON.stringify(details));
-        console.log(details);
-      }
+  const tryConnect = async () => {
+    while (!ws || ws.readyState !== wsState.OPEN) {
+      console.log('try connecting...');
+      ws = new WebSocket('ws://localhost:49367/ws');
+      wsList[tab.id] = ws;
+      await sleep(5000);
     }
-  },{
-    types: ['xmlhttprequest'],
-    urls: ["<all_urls>"],
-  },
-  ['requestHeaders', 'extraHeaders']
-);
+    
+    console.log('connected!');
+  
+    ws.onclose = () => {
+      console.log('disconnected!');
+      tryConnect();
+    };
+  }
+
+  tryConnect();
+
+  // this api is asynchronous
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+      if (ws && ws.readyState === wsState.OPEN) {
+        if (!videoList[details.url] && details.url.includes('m3u8')) {
+          videoList[details.url] = true;
+          ws.send(JSON.stringify(details));
+          console.log(details);
+        }
+      }
+    },{
+      tabId: tab.id,
+      types: ['xmlhttprequest'],
+      urls: ["<all_urls>"]
+    },
+    ['requestHeaders', 'extraHeaders']
+  );
+});
+
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  const ws = wsList[tabId];
+  if (ws) {
+    ws.close();
+    delete wsList[tabId];
+  }
+});
