c20a1c1d548b8d25aeb362337f4be746f5e87c22
fatzchang <fatz.zhang@gmail.com>
2022-05-06 22:39:49 +0800

    keep service worker alive ignore iframe's request

--- src/background_script.js
+++ src/background_script.js
@@ -4,10 +4,8 @@ const wsState = {
   CLOSING: 2,
   CLOSED: 3
 };
-
+ 
 wsList = {}; // group by tabid
-
-
 
 chrome.tabs.onCreated.addListener((tab) => {
   console.log('tab is created!');
@@ -15,18 +13,30 @@ chrome.tabs.onCreated.addListener((tab) => {
 
   const tryConnect = () => {  
     console.log(tab.id, 'trying connect...');
-    wsList[tab.id] = new WebSocket('ws://localhost:49367/ws'),
+    wsList[tab.id] = new WebSocket('ws://localhost:49367/ws');
 
+    // let timer;
     wsList[tab.id].onopen = () => {
       console.log('connected!');
+      wsList[tab.id].send('ping');
+    }
+
+    wsList[tab.id].onmessage = (message) => {
+      setTimeout(() => {
+        if (wsList[tab.id]) {
+          wsList[tab.id].send('ping');
+        }
+      }, 5000);
     }
 
     wsList[tab.id].onclose = () => {
       console.log('disconnected!');
+      delete wsList[tab.id];
     };
 
     wsList[tab.id].onerror = () => {
       console.log('error!');
+
       if (wsList[tab.id]) {
         tryConnect();
       }
@@ -38,12 +48,14 @@ chrome.tabs.onCreated.addListener((tab) => {
   // this api is asynchronous
   chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
-      let ws = wsList[tab.id];
-      if (ws && ws.readyState === wsState.OPEN) {
-        if (!videoList[details.url] && details.url.includes('m3u8')) {
-          videoList[details.url] = true;
-          ws.send(JSON.stringify(details));
-          console.log(details);
+      // ignore iframe's request
+      if (details.parentFrameId === -1) {
+        let ws = wsList[tab.id];
+        if (ws && ws.readyState === wsState.OPEN) {
+          if (!videoList[details.url] && details.url.includes('m3u8')) {
+            videoList[details.url] = true;
+            ws.send(JSON.stringify(details));
+          }
         }
       }
     },{
@@ -55,10 +67,50 @@ chrome.tabs.onCreated.addListener((tab) => {
   );
 });
 
-
 chrome.tabs.onRemoved.addListener((tabId) => {
   if (wsList[tabId]) {
     wsList[tabId].close();
-    delete wsList[tabId];
   }
 });
+
+
+// mechanism to keep service worker alive in background
+let lifeline;
+
+keepAlive();
+
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name === 'keepAlive') {
+    lifeline = port;
+    setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds
+    port.onDisconnect.addListener(keepAliveForced);
+  }
+});
+
+function keepAliveForced() {
+  lifeline?.disconnect();
+  lifeline = null;
+  keepAlive();
+}
+
+async function keepAlive() {
+  if (lifeline) return;
+  for (const tab of await chrome.tabs.query({ url: '*://*/*' })) {
+    try {
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => chrome.runtime.connect({ name: 'keepAlive' }),
+        // `function` will become `func` in Chrome 93+
+      });
+      chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
+      return;
+    } catch (e) {}
+  }
+  chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
+}
+
+async function retryOnTabUpdate(tabId, info, tab) {
+  if (info.url && /^(file|https?):/.test(info.url)) {
+    keepAlive();
+  }
+}
